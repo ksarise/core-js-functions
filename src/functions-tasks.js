9fa9e3aa1223386a5afa803e62a28b8db6c12991
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-04-13 03:02:10 +0500

    finish partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -172,8 +172,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args) {
+  return (...partArgs) => fn(...args, ...partArgs);
 }
 
 /**
