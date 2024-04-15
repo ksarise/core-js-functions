e25616dfff7294ecf3e4d91a36d3cf063c203aa2
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-04-16 01:48:46 +0500

    feat: finish retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -146,8 +146,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function () {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        if (i === attempts - 1) {
+          throw new Error('test');
+        }
+      }
+    }
+    return undefined;
+  };
 }
 
 /**
